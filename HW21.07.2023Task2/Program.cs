3fb4fc2cc374b3d4921c2f72a0ed906e4bc0e279
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-10-21 18:45:52 +0300

    Task 2 Done

---
+++ HW21.07.2023Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program 
+{
+    delegate double Function(double x);
+    static void Main() 
+    {
+        double a = 3.0; double b = 4.0; double c = 5.0;
+        Function qDelegate = MakeFunction(a, b, c);
+        double x1 = 2.0;
+        double result1 = qDelegate(x1);
+        Console.WriteLine($"Result if x = {x1}: {result1}");
+        double x2 = 3.0;
+        double result2 = qDelegate(x2);
+        Console.WriteLine($"Result if x = {x2}: {result2}");
+    }
+    static Function MakeFunction(double a, double b, double c) 
+    {
+        Function func = (x) => a * x * x + b * x + c;
+        return func;
+    }
+}
