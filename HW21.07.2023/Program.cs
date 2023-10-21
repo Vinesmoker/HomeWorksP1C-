4b56430fb7e2ba42428ecfabe51b90b6099e06f6
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-10-21 18:32:00 +0300

    Task 1 done

---
+++ HW21.07.2023/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program 
+{
+    delegate string GetDayOfWeek();
+    static void Main() 
+    {
+        string[] days = { "Sanday", "Monday", "Tuesday", "Wednesday", "Thursday", "Ftiday", "Saturday" };
+        int id = 0;
+        GetDayOfWeek getNextDay = delegate
+        {
+            string nextDay = days[id];
+            id = (id + 1) % days.Length;
+            return nextDay;
+        };
+        for (int i = 0; i < 25; i++) 
+        {
+            string nextDay = getNextDay();
+            Console.WriteLine(nextDay);
+        }
+    }
+}
