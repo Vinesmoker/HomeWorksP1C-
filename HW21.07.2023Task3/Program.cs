c11af9711d2ea81d6d4d3fd397823173ce7b863a
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2023-10-21 19:29:15 +0300

    Task 3 Done

---
+++ HW21.07.2023Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿class AutoService
+{
+    public event Action<string> ServiceEvent = delegate { };
+    private readonly string serviceName;
+    public AutoService(string name) => serviceName = name;
+    public void GenerateEvent()
+    {
+        string message = $"Your car has been repaired and you can pick it up at any time! {serviceName}";
+        ServiceEvent.Invoke(message);
+    }
+}
+class EventHandler
+{
+    private readonly string handlerName;
+    public EventHandler(string name)
+    {
+        handlerName = name;
+    }
+    public void HandleEvent(string message)
+    {
+        Console.WriteLine($"Client {handlerName} get a message: {message}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        AutoService toyota = new ("Toyota");
+        AutoService audi = new ("Audi");
+        EventHandler eventHandler = new ("Bob");
+        toyota.ServiceEvent += eventHandler.HandleEvent;
+        audi.ServiceEvent += eventHandler.HandleEvent;
+        toyota.GenerateEvent();
+        audi.GenerateEvent();
+        Console.ReadLine();
+    }
+}
